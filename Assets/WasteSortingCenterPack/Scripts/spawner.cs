3d31168aaf79fe8c38992786321563edb7d13a95
Baptiste-DLDA <baptdelaverny@gmail.com>
2025-11-06 12:20:02 +0100

    test modif

--- Assets/WasteSortingCenterPack/Scripts/spawner.cs
+++ Assets/WasteSortingCenterPack/Scripts/spawner.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class spawner : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public bool isActive = true;
+    public bool isActive = false;
     public GameObject ObjectToSpawn;
     void Start()
     {
