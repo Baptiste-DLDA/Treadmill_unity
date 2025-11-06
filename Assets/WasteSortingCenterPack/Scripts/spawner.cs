5e5028cc081486e377f44abc98f50c5188d36103
Baptiste-DLDA <baptdelaverny@gmail.com>
2025-11-06 12:14:02 +0100

    first commit

---
+++ Assets/WasteSortingCenterPack/Scripts/spawner.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public bool isActive = true;
+    public GameObject ObjectToSpawn;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
